e1f1cde7446cc454498cbc57168050256d5f4687
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-07-12 12:26:24 +0200

    Simplify codes for protectedroute

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components';
+import ProtectedRoute from '../context/ProtectedRoute';
 
 const UserLayout = () => {
     return (
-        <div className="min-h-screen bg-gray-100">
-            <Header />
-            <main className="px-4 mx-auto max-w-1xl sm:px-6 lg:px-8">
-                <Outlet />
-            </main>
-        </div>
+        <ProtectedRoute>
+            <div className="min-h-screen bg-gray-100">
+                <Header />
+                <main className="px-4 mx-auto max-w-1xl sm:px-6 lg:px-8">
+                    <Outlet />
+                </main>
+            </div>
+        </ProtectedRoute>
     );
 };
 
