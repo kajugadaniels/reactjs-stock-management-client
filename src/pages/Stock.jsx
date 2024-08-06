5af22c43385c689f68fa012acbef5e26d41d7b18
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-06 11:11:27 +0200

    New changes on stock.jsx

--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -103,7 +103,27 @@ const Stock = () => {
                         </div>
                     </div>
                 </Link>
-                {/* Other links and stats... */}
+                <Link to='/StockOut'>
+                    <div className="w-32 p-2 text-white bg-purple-500 rounded-lg">
+                        <div className="flex items-center ">
+                            <div>
+                                <div className='flex gap-1'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                                        <g fill="none">
+                                            <path d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z"></path>
+                                            <path fill="white" d="M5 6a1 1 0 0 0-2 0v12a1 1 0 1 0 2 0zm7.703 10.95a1 1 0 0 0 0-1.415L10.167 13H20a1 1 0 1 0 0-2h-9.833l2.536-2.536a1 1 0 0 0-1.415-1.414l-4.242 4.243a1 1 0 0 0 0 1.414l4.242 4.243a1 1 0 0 0 1.415 0"></path>
+                                        </g>
+                                    </svg>
+                                    <div className="text-xs font-bold">Stock Out</div>
+                                </div>
+                                <div className='bg-[#ebfdfe] px-2 py-3 mt-1 rounded-lg'>
+                                    <div className="text-lg font-bold text-purple-500">79 T</div>
+                                    <div className="pt-1 pr-1 text-xs text-gray-500">100 Packaging</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
             </div>
 
             <div className="flex flex-wrap items-center mb-6 space-y-2 md:space-y-0 md:space-x-4">
@@ -141,15 +161,6 @@ const Stock = () => {
                         Generate Report
                     </div>
                 </button>
-                <div className="flex items-center space-x-2">
-                    <label className="text-sm">From</label>
-                    <input type="date" className="p-2 border rounded-lg border-zinc-300" defaultValue="2024-02-09" />
-                </div>
-                <div className="flex items-center space-x-2">
-                    <label className="text-sm">To</label>
-                    <input type="date" className="p-2 border rounded-lg border-zinc-300" defaultValue="2024-02-20" />
-                </div>
-                <button className="px-4 py-2 text-sm text-white bg-green-500 rounded-lg">Today</button>
             </div>
 
             <div className="overflow-x-auto">
